0929bacebaa8d4306fb6aab7625cae1a2f90fd9d
PgmAdmin <prmorais1302@gmail.com>
2023-09-08 22:27:44 -0300

    Adding and configuring submit button

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  email = '';
+  password = '';
+
+  onSubmit(form: NgForm) {
+    console.log('onSubmit ' + JSON.stringify(form.value));
+  }
+}
