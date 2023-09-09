dbb389a411833ad7e65a153499920a6b94a42fb4
PgmAdmin <prmorais1302@gmail.com>
2023-09-08 21:02:02 -0300

    Adding Components and routes

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [],
+  exports: [MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
---
+++ src/app/training/current-training/current-training.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-training',
+  templateUrl: './current-training.component.html',
+  styleUrls: ['./current-training.component.css'],
+})
+export class CurrentTrainingComponent {}
