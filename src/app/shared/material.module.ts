1778f3e15085bb315f10cd90304e8aea6807c3e0
PgmAdmin <prmorais1302@gmail.com>
2023-09-08 21:17:52 -0300

    Creating signup page

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  exports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
